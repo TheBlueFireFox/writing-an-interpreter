c1fcd155d9e19b96445661c5aa14d2d48a022dfa
Adrian Hornung <adrian.hornung@yahoo.de>
2023-08-07 03:15:11 +0200

    Rust: Until If Expressions

--- rust/interpreter/src/ast.rs
+++ rust/interpreter/src/ast.rs
@@ -1,9 +1,20 @@
-#[derive(Debug, PartialEq, Eq)]
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Program<'input> {
     pub statements: Vec<Statement<'input>>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+impl Display for Program<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for statement in &self.statements {
+            write!(f, "{statement}")?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Statement<'input> {
     Let(&'input str, Expression<'input>),
     Return(Expression<'input>),
@@ -12,10 +23,27 @@ pub enum Statement<'input> {
     Invalid,
 }
 
+impl Display for Statement<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Statement::Let(name, expr) => write!(f, "let {name} = {expr};"),
+            Statement::Return(expr) => write!(f, "return {expr};"),
+            Statement::Expression(expr) => write!(f, "{expr}"),
+            Statement::Invalid => write!(f, "invalid Statement -.-;"),
+            Statement::Block(exprs) => {
+                for expr in exprs {
+                    write!(f, "{expr}")?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
 pub type BS<'i> = Box<Statement<'i>>;
 pub type BE<'i> = Box<Expression<'i>>;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Expression<'input> {
     Ident(&'input str),
     Int(i64),
@@ -35,3 +63,57 @@ pub enum Expression<'input> {
     Call(BE<'input>, Vec<Expression<'input>>),
     Invalid,
 }
+
+impl Display for Expression<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let single = |f: &mut std::fmt::Formatter<'_>, sym, expr| write!(f, "({}{})", sym, expr);
+        let two = |f: &mut std::fmt::Formatter<'_>, sym, l, r| write!(f, "({} {} {})", l, sym, r);
+        let param = |p: &[Expression<'_>]| {
+            p.iter()
+                .map(|expr| format!("{expr}"))
+                .collect::<Vec<_>>()
+                .join(", ")
+        };
+        let alt_cond = |alt: &Option<BE<'_>>| {
+            alt.as_ref()
+                .map(|c| format!("else {c}"))
+                .unwrap_or_default()
+        };
+
+        match self {
+            Expression::Ident(lit) => write!(f, "{lit}"),
+            Expression::Int(lit) => write!(f, "{lit}"),
+            Expression::Bool(lit) => write!(f, "{lit}"),
+            Expression::Neg(expr) => single(f, "-", expr),
+            Expression::Not(expr) => single(f, "!", expr),
+            Expression::Add(l, r) => two(f, "+", l, r),
+            Expression::Min(l, r) => two(f, "-", l, r),
+            Expression::Mul(l, r) => two(f, "*", l, r),
+            Expression::Div(l, r) => two(f, "/", l, r),
+            Expression::Neq(l, r) => two(f, "!=", l, r),
+            Expression::Eq(l, r) => two(f, "==", l, r),
+            Expression::Gt(l, r) => two(f, ">", l, r),
+            Expression::Le(l, r) => two(f, "<", l, r),
+            Expression::If(cond, cons, alt) => write!(f, "if {cond} {cons}{}", alt_cond(alt)),
+            Expression::Fn(params, blk) => write!(f, "fn({}){}", param(params), blk),
+            Expression::Call(fun, params) => write!(f, "{}({})", fun, param(params)),
+            Expression::Invalid => write!(f, "InvalidExpression"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display_let() {
+        let program = Program {statements : vec![
+            Statement::Let("myVar", Expression::Ident("anotherVar"))
+        ]};
+        let program = format!("{program}");
+
+        assert_eq!("let myVar = anotherVar;", program);
+    }
+}
+
--- rust/interpreter/src/parser.rs
+++ rust/interpreter/src/parser.rs
@@ -1,5 +1,5 @@
 use crate::{
-    ast::{Expression, Program, Statement},
+    ast::{Expression, Program, Statement, BE},
     lexer::tokenize,
     token::Token,
 };
@@ -9,6 +9,33 @@ pub fn parse(input: &str) -> Result<Program, Vec<String>> {
     Parser::new(&tokens)
         .parse()
         .map(|s| Program { statements: s })
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Predecence {
+    Lowest,
+    Equals,
+    LessGreater,
+    Sum,
+    Product,
+    Prefix,
+    Call,
+}
+
+impl Predecence {
+    fn map(token: &Token) -> Self {
+        match token {
+            Token::Eq => Predecence::Equals,
+            Token::Neq => Predecence::Equals,
+            Token::Lt => Predecence::LessGreater,
+            Token::Gt => Predecence::LessGreater,
+            Token::Plus => Predecence::Sum,
+            Token::Minus => Predecence::Sum,
+            Token::Slash => Predecence::Product,
+            Token::Asterisk => Predecence::Product,
+            t => Predecence::Lowest,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -33,9 +60,7 @@ impl<'toks, 'input> Parser<'toks, 'input> {
                     self.advance(1);
                     break;
                 }
-                Some(_) => {
-                    todo!()
-                }
+                Some(_) => self.expression_statement(),
                 None => Err("Missing EOF".to_string()),
             };
 
@@ -74,6 +99,16 @@ impl<'toks, 'input> Parser<'toks, 'input> {
         }
     }
 
+    fn expression_statement(&mut self) -> Result<Statement<'input>, String> {
+        let statement = self.parse_expression(Predecence::Lowest)?;
+
+        if let Token::Semicolon = self.tokens[0] {
+            self.advance(1);
+        }
+
+        Ok(Statement::Expression(statement))
+    }
+
     fn let_statement(&mut self) -> Result<Statement<'input>, String> {
         match self.tokens {
             &[Token::Let, Token::Ident(name), Token::Assign, ..] => {
@@ -98,13 +133,114 @@ impl<'toks, 'input> Parser<'toks, 'input> {
         self.advance_after_semicolon();
         Ok(Statement::Return(Expression::Invalid))
     }
+
+    fn parse_expression(&mut self, pred: Predecence) -> Result<Expression<'input>, String> {
+        // TODO:
+        let mut left = self.parse_prefix()?;
+
+        loop {
+            if let Token::Semicolon | Token::Eof = &self.tokens[0] {
+                break;
+            }
+
+            let curr_pred = Predecence::map(&self.tokens[0]);
+            if pred >= curr_pred {
+                break;
+            }
+
+            left = self.parse_infix_expression(left)?;
+        }
+
+        Ok(left)
+    }
+
+    fn parse_prefix(&mut self) -> Result<Expression<'input>, String> {
+        match self.tokens.get(0) {
+            Some(&Token::Ident(ident)) => self.parse_ident(ident),
+            Some(&Token::Int(val)) => self.parse_int(val),
+            Some(Token::Bang) => self.parse_not(),
+            Some(Token::Minus) => self.parse_neg(),
+            Some(Token::False) => self.parse_bool(false),
+            Some(Token::True) => self.parse_bool(true),
+            Some(Token::If) => todo!(),
+            Some(Token::Fun) => todo!(),
+            Some(Token::LParen) => self.parse_grouped(),
+            Some(token) => Err(format!("Illegal token <{:?}> is not a prefix", token)),
+            None => Err("No toke for a prefix found".to_string()),
+        }
+    }
+
+    fn parse_infix_expression(
+        &mut self,
+        left: Expression<'input>,
+    ) -> Result<Expression<'input>, String> {
+        type Runner<'a> = dyn Fn(BE<'a>, BE<'a>) -> Expression<'a>;
+        let runner: &Runner = match self.tokens[0] {
+            Token::Plus => &|l, r| Expression::Add(l, r),
+            Token::Minus => &|l, r| Expression::Min(l, r),
+            Token::Slash => &|l, r| Expression::Div(l, r),
+            Token::Asterisk => &|l, r| Expression::Mul(l, r),
+            Token::Eq => &|l, r| Expression::Eq(l, r),
+            Token::Neq => &|l, r| Expression::Neq(l, r),
+            Token::Lt => &|l, r| Expression::Le(l, r),
+            Token::Gt => &|l, r| Expression::Gt(l, r),
+            Token::LParen => todo!(),
+            _ => return Ok(left),
+        };
+
+        let curr = &self.tokens[0];
+        self.advance(1);
+
+        let right = self.parse_expression(Predecence::map(curr))?;
+
+        Ok(runner(Box::new(left), Box::new(right)))
+    }
+
+    fn parse_grouped(&mut self) -> Result<Expression<'input>, String> {
+        println!("foo {:?}",self.tokens);
+        self.advance(1);
+        let expr = self.parse_expression(Predecence::Lowest)?;
+
+        println!("foo {:?}",self.tokens);
+        if let Token::RParen = self.tokens[0] {
+            self.advance(1);
+            return Ok(expr);
+        }
+        Err("Missing Right Paren )".to_string())
+    }
+
+    fn parse_ident(&mut self, ident: &'input str) -> Result<Expression<'input>, String> {
+        self.advance(1);
+        Ok(Expression::Ident(ident))
+    }
+
+    fn parse_int(&mut self, val: i64) -> Result<Expression<'input>, String> {
+        self.advance(1);
+        Ok(Expression::Int(val))
+    }
+
+    fn parse_bool(&mut self, val: bool) -> Result<Expression<'input>, String> {
+        self.advance(1);
+        Ok(Expression::Bool(val))
+    }
+
+    fn parse_not(&mut self) -> Result<Expression<'input>, String> {
+        self.advance(1);
+        let inner = self.parse_expression(Predecence::Prefix)?;
+        Ok(Expression::Not(Box::new(inner)))
+    }
+
+    fn parse_neg(&mut self) -> Result<Expression<'input>, String> {
+        self.advance(1);
+        let inner = self.parse_expression(Predecence::Prefix)?;
+        Ok(Expression::Neg(Box::new(inner)))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::ast::{Expression, Statement};
-
     use super::*;
+    use crate::ast::{Expression, Statement, BE};
 
     fn eq(input: &str, exp: &[Statement<'_>]) {
         let got = parse(input);
@@ -156,5 +292,133 @@ mod tests {
         ];
 
         eq(input, &exp);
+    }
+
+    #[test]
+    fn test_infix_expressions() {
+        let input = "foobar;";
+        let exp = [Statement::Expression(Expression::Ident("foobar"))];
+        eq(input, &exp);
+    }
+
+    #[test]
+    fn test_int_expressions() {
+        let input = "5;";
+        let exp = [Statement::Expression(Expression::Int(5))];
+        eq(input, &exp);
+    }
+
+    #[test]
+    fn test_prefix_int_with_semi() {
+        let input = ["!5;", "-15;"];
+        let exp = [
+            [Statement::Expression(Expression::Not(Box::new(
+                Expression::Int(5),
+            )))],
+            [Statement::Expression(Expression::Neg(Box::new(
+                Expression::Int(15),
+            )))],
+        ];
+
+        for (i, e) in std::iter::zip(&input, &exp) {
+            eq(i, e)
+        }
+    }
+
+    #[test]
+    fn test_prefix_int_no_semi() {
+        let input = ["!5", "-15"];
+        let exp = [
+            [Statement::Expression(Expression::Not(Box::new(
+                Expression::Int(5),
+            )))],
+            [Statement::Expression(Expression::Neg(Box::new(
+                Expression::Int(15),
+            )))],
+        ];
+
+        for (i, e) in std::iter::zip(&input, &exp) {
+            eq(i, e)
+        }
+    }
+
+    #[test]
+    fn test_parsing_bool() {
+        type Runner<'a> = dyn Fn(BE<'a>, BE<'a>) -> Expression<'a>;
+        let exp: &[&Runner] = &[
+            &|l, r| Expression::Eq(l, r),
+            &|l, r| Expression::Neq(l, r),
+            &|l, r| Expression::Neq(l, r),
+            &|l, r| Expression::Eq(l, r),
+        ];
+
+        let input = [
+            ("true == true", true, true),
+            ("true != false;", true, false),
+            ("false != true;", false, true),
+            ("false == false", false, false),
+        ];
+
+        let make = |v| Box::new(Expression::Bool(v));
+        for (&(input, l, r), fun) in std::iter::zip(&input, exp) {
+            let exp = fun(make(l), make(r));
+            let exp = Statement::Expression(exp);
+            eq(input, &[exp]);
+        }
+    }
+
+    #[test]
+    fn test_parsing_infix_expression() {
+        type Expr<'a> = dyn Fn(BE<'a>, BE<'a>) -> Expression<'a>;
+        let cons: &[(&str, &Expr)] = &[
+            ("5 + 5;", &|l, r| Expression::Add(l, r)),
+            ("5 - 5;", &|l, r| Expression::Min(l, r)),
+            ("5 * 5;", &|l, r| Expression::Mul(l, r)),
+            ("5 / 5;", &|l, r| Expression::Div(l, r)),
+            ("5 > 5;", &|l, r| Expression::Gt(l, r)),
+            ("5 < 5;", &|l, r| Expression::Le(l, r)),
+            ("5 == 5;", &|l, r| Expression::Eq(l, r)),
+            ("5 != 5;", &|l, r| Expression::Neq(l, r)),
+        ];
+        let five = Box::new(Expression::Int(5));
+        for (input, gen) in cons {
+            let exp = gen(five.clone(), five.clone());
+            let exp = Statement::Expression(exp);
+            eq(input, &[exp]);
+        }
+    }
+
+    #[test]
+    fn test_operator_pred() {
+        let lst = [
+            ("-a * b", "((-a) * b)"),
+            ("!-a", "(!(-a))"),
+            ("a + b + c", "((a + b) + c)"),
+            ("a + b - c", "((a + b) - c)"),
+            ("a * b * c", "((a * b) * c)"),
+            ("a * b / c", "((a * b) / c)"),
+            ("a + b / c", "(a + (b / c))"),
+            ("a + b * c + d / e - f", "(((a + (b * c)) + (d / e)) - f)"),
+            ("3 + 4; -5 * 5", "(3 + 4)((-5) * 5)"),
+            ("5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))"),
+            ("5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))"),
+            (
+                "3 + 4 * 5 == 3 * 1 + 4 * 5",
+                "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+            ),
+            ("true", "true"),
+            ("false", "false"),
+            ("3 > 5 == false", "((3 > 5) == false)"),
+            ("3 < 5 == true", "((3 < 5) == true)"),
+            ("1 + (2 + 3) + 4", "((1 + (2 + 3)) + 4)"),
+            ("(5 + 5) * 2", "((5 + 5) * 2)"),
+            ("2 / (5 + 5)", "(2 / (5 + 5))"),
+            ("-(5 + 5)", "(-(5 + 5))"),
+            ("!(true == true)", "(!(true == true))"),
+        ];
+
+        for (input, exp) in lst {
+            assert_eq!(exp, format!("{}", parse(input).unwrap()));
+        }
     }
 }
